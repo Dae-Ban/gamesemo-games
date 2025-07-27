1652f0569cf7104dc94d38e66c408c21e2f11564
성찬 <ysc1086@gmail.com>
2025-07-27 14:28:18 +0900

    api응답 받기

--- frontend/src/services/gameInfoService.ts
+++ frontend/src/services/gameInfoService.ts
@@ -1,7 +1,23 @@
 import axios from 'axios';
+import { ApiResponse } from '../types/ApiResponse';
 import { GameInfo } from '../types/GameInfo';
 
 export const fetchGameList = async (count: number): Promise<GameInfo[]> => {
-  const response = await axios.get<GameInfo[]>(`localhost/game/list/${count}`);
-  return response.data;
+  const response = await axios.get<ApiResponse<GameInfo[]>>(`http://localhost/game/list?size=${count}`);
+  
+  if (!response.data.success) {
+    throw new Error(response.data.message);
+  }
+
+  return response.data.data;
+};
+
+export const fetchGameInfo = async (gNum: number): Promise<GameInfo[]> => {
+  const response = await axios.get<ApiResponse<GameInfo[]>>(`http://localhost/game/${gNum}`);
+  
+  if (!response.data.success) {
+    throw new Error(response.data.message);
+  }
+
+  return response.data.data;
 };
